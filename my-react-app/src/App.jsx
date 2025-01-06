4a3b359dd1e9f1435b4485333800c4a66566436c
Manish Kumar <manish356111@gmail.com>
2025-01-07 00:34:40 +0530

    added props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,10 +3,16 @@ import Footer from "./Footer.jsx"
 import Food from "./Food.jsx"
 import Card from "./Card.jsx"
 import Button from "./Button.jsx"
+import Student from "./Student.jsx"
 
 function App() {
  return(
   <>
+  <Student name="Spongebob" age={30} isStudent={true}></Student>
+  <Student name="Patrick" age={42} isStudent={false}></Student>
+  <Student name="Squid" age={50} isStudent={false}></Student>
+  <Student name="Sandy" age={27} isStudent={true}></Student>
+  <Student></Student>
   <Card/>
   <Button/>
   <Header></Header>
