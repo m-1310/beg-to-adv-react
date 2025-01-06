470c60010b90ccdafe4a2bfb6d5cba045cbdb9bf
Manish Kumar <manish356111@gmail.com>
2025-01-07 00:47:14 +0530

    added props with conditional rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,10 +4,13 @@ import Food from "./Food.jsx"
 import Card from "./Card.jsx"
 import Button from "./Button.jsx"
 import Student from "./Student.jsx"
+import UserGreeting from "./userGreeting.jsx"
 
 function App() {
  return(
   <>
+  <UserGreeting isLoggedIn={true} username="Manish"></UserGreeting>
+  <UserGreeting isLoggedIn={false} username="Manish"></UserGreeting>
   <Student name="Spongebob" age={30} isStudent={true}></Student>
   <Student name="Patrick" age={42} isStudent={false}></Student>
   <Student name="Squid" age={50} isStudent={false}></Student>
---
+++ my-react-app/src/userGreeting.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+function UserGreeting(props){
+    const welcomeMessage = <h2 className="welcome-message">Welcome {props.username}</h2>;
+    const loginPromt =  <h2 className="login-promt">Please login in to Continue</h2>;
+
+    if(props.isLoggedIn){ 
+      return(
+        welcomeMessage
+      );
+    }else{
+      return(
+        loginPromt
+      );
+    }
+}
+UserGreeting.propTypes={
+  isLoggedIn: PropTypes.bool,
+  username: PropTypes.string,
+}
+Username.defaultProps = {
+  isLoggedIn: false,
+  username: "Guest",
+}
+
+export default UserGreeting;
