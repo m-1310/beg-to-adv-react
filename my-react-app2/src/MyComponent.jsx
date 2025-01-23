1b28ddc7d646f524d442c50e7628d74ea43c1ee2
Manish Kumar <manish356111@gmail.com>
2025-01-24 03:08:35 +0530

    event handler onchange

--- my-react-app2/src/MyComponent.jsx
+++ my-react-app2/src/MyComponent.jsx
@@ -4,6 +4,9 @@ function MyComponent(){
 
   const [name, setName] = useState("Guest");
   const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
 
   function handleNameChange(event){
     setName(event.target.value);
@@ -13,14 +16,53 @@ function MyComponent(){
     setQuantity(event.target.value);
   }
 
+  function handleCommentChange(event){
+    setComment(event.target.value)
+  }
+
+  function handlePaymentChange(event){
+    setPayment(event.target.value)
+  }
+
+  function handleShippingChange(event){
+    setShipping(event.target.value)
+  }
+
   return(
     <div>
       <input value={name} onChange={handleNameChange}>
       </input>
       <p>Name: {name}</p>
+
       <input value={quantity} onChange={handleQuantityChange} type="number">
       </input>
       <p>Quantity: {quantity}</p>
+
+      <textarea value={comment} onChange={handleCommentChange}
+      placeholder="Enter Delivery Instructions"></textarea>
+      <p>Comment: {comment}</p>
+
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an Option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">Mastercard</option>
+        <option value="Giftcard">Gift Card</option>
+      </select>
+      <p>Payment: {payment}</p>
+
+      <label>
+        <input type="radio" value="Pick Up" 
+        checked={shipping === "Pick Up"}
+        onChange={handleShippingChange}></input>
+        Pick Up
+      </label><br/>
+      <label>
+      <input type="radio" value="Delivery" 
+        checked={shipping === "Delivery"}
+        onChange={handleShippingChange}></input>
+        Delivery
+      </label>
+      <p>Shipping Options: {shipping}</p>
     </div>
   )
 }
