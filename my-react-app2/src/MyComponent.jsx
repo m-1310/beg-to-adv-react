59547bcd176a0533893512602a523a33ab7a4f76
Manish Kumar <manish356111@gmail.com>
2025-01-24 02:56:13 +0530

    onChange

---
+++ my-react-app2/src/MyComponent.jsx
@@ -0,0 +1,28 @@
+import React,{useState} from "react"
+
+function MyComponent(){
+
+  const [name, setName] = useState("Guest");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleNameChange(event){
+    setName(event.target.value);
+  }
+
+  function handleQuantityChange(event){
+    setQuantity(event.target.value);
+  }
+
+  return(
+    <div>
+      <input value={name} onChange={handleNameChange}>
+      </input>
+      <p>Name: {name}</p>
+      <input value={quantity} onChange={handleQuantityChange} type="number">
+      </input>
+      <p>Quantity: {quantity}</p>
+    </div>
+  )
+}
+
+export default MyComponent;
