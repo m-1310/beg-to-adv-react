c670f50f638432d2dd37d9ac4d60b44fa7d3382c
Manish Kumar <manish356111@gmail.com>
2025-02-04 02:22:38 +0530

    added a component

--- react-app/src/MyComponent.jsx
+++ react-app/src/MyComponent.jsx
@@ -1,9 +1,21 @@
 import React,{useState, useEffect, useRef} from 'react';
 
 function MyComponent(){
-  
+
+  const ref = useRef(0);
+
+  useEffect(()=>{
+    console.log("Comp. Rendered");
+  });
+
+
+  function handleClick(){
+    ref.current = ref.current+1;
+    console.log(ref.current);
+  }
+
   return(<>
-  
+    <button onClick={handleClick}>Click Me!</button>
   </>)
 }
 
