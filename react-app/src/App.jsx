3d2340c9bce9fc3fbfb4269b2ecd2a1728a68d0e
Manish Kumar <manish356111@gmail.com>
2025-02-04 02:13:55 +0530

    Prop Drilling alt. useContext

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,13 @@
+import ComponentA from "./ComponentA.jsx"
+import MyComponent from "./MyComponent.jsx"
 
 function App() {
 
   return (
     <>
-  
+    <ComponentA></ComponentA>
+    <br/> <br/> <br/> <br/> 
+    <MyComponent></MyComponent>
     </>
   )
 }
---
+++ react-app/src/MyComponent.jsx
@@ -0,0 +1,10 @@
+import React,{useState, useEffect, useRef} from 'react';
+
+function MyComponent(){
+  
+  return(<>
+  
+  </>)
+}
+
+export default MyComponent;
